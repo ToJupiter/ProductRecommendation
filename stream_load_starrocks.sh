81a821c869ddcb0c23a03517eb1c5f81b332911e
D3etMe4n <D3etMe4n@gmail.com>
2025-02-11 18:50:05 +0700

    add starrocks stream load bash file

---
+++ stream_load_starrocks.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DATABASE="product_recommendation"
+TABLE="data"
+STARROCKS_USER="root"
+STARROCKS_PASSWORD=""
+FE_HOST="localhost:8030"
+DATA_DIR="./data.csv" # replace with your file destination
+
+# Optional label for this load
+LABEL="load_product_recommendation_data_final"
+
+# Check that the file exists before loading
+if [ -f "$DATA_DIR" ]; then
+    curl --location-trusted -u "$STARROCKS_USER:$STARROCKS_PASSWORD" \
+         -H "Expect:100-continue" \
+         -H "label: ${LABEL}" \
+         -H "column_separator: ," \
+         -H "line_delimiter: \n" \
+         -H "max_filter_ratio: 1" \
+         -T "$DATA_DIR" \
+         -XPUT "http://$FE_HOST/api/$DATABASE/$TABLE/_stream_load"
+else
+    echo "File not found: $DATA_DIR , make sure the data file is in the same directory with this script"
+fi
